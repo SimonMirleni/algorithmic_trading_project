a2dae5f0732f73d19f4c74de91cbca2f06df4044
SaimonMirleni <smirleni@frba.utn.edu.ar>
2024-03-19 19:56:33 -0300

    Deleting Dividends and Stock Splits Columns. And adding Tomorrow and Target columns. Deleting data before 1990.

--- stock_market_predictor.py
+++ stock_market_predictor.py
@@ -11,3 +11,11 @@ sp500 = sp500.history(period = "max")
 sp500.plot.line(y="Close", use_index = True)
 plt.show()
 
+del sp500["Dividends"]
+del sp500["Stock Splits"]
+sp500["Tomorrow"] = sp500["Close"].shift(-1)
+sp500["Target"] = (sp500["Tomorrow"]>sp500["Close"]).astype(int)
+
+#I will take data from 1990 to now because with stock market data if you go back too far, 
+#the market could have shifted fundamentally and some of that old data may not be as useful to make future predictions.
+sp500 = sp500.loc["1990-01-01":].copy()
